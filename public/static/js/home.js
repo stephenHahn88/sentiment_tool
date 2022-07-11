5de8545f453ed7ab56549dced0fe7d639cfb093f
stephenHahn88 <89817088+stephenHahn88@users.noreply.github.com>
2022-07-11 14:19:31 -0400

    server functionality
    added server and database

---
+++ public/static/js/home.js
@@ -0,0 +1,151 @@
+let valenceInterval = null;
+let currentEmotion = null;
+
+document.addEventListener('keyup', e => {
+    let audio = document.getElementById("audio")
+  if ((e.key === " " || e.code === "Space") &&  !audio.paused) {
+    let p = document.getElementById("audio-measures");
+    p.innerHTML += audio.currentTime + ",<br/>"
+  }
+})
+
+function startAnnotate() {
+    // annotates current valence value every 100 ms
+    valenceInterval = setInterval(annotate, 100);
+    $("#audio-options").attr("disabled", true)
+}
+
+function stopAnnotate() {
+    // stops the annotation process
+    if (valenceInterval != null) {
+        clearInterval(valenceInterval)
+    }
+    valenceInterval = null;
+}
+
+function annotate() {
+    // appends the current time, valence pair to a p tag
+    let audio = document.getElementById("audio")
+    let p = document.getElementById("audio-valence")
+    // let i = document.getElementById("valence-slider")
+    if (!audio.paused) {
+        p.innerHTML += audio.currentTime + " : " + currentEmotion + ",<br/>"
+    }
+}
+
+function clickEmotion(emotion) {
+    currentEmotion = emotion
+}
+
+function restartAnnotate(interval) {
+    // resets the time, valence pairs and stops annotation
+    stopAnnotate();
+
+    let p;
+    let tag;
+    if (interval === valenceInterval) {
+        p = document.getElementById("audio-valence")
+        tag = "valence"
+    } else {
+        p = document.getElementById("audio-measures")
+        tag = "measures"
+    }
+    e = document.getElementById("audio-options");
+    new_file = e.options[e.selectedIndex].text
+    $("#audio-options").attr("disabled", false)
+    p.innerHTML = new_file.substring(0, new_file.length - 4) + "_" + tag + " = {"
+}
+
+function newAudio() {
+    // loads new audio when a new melody is selected
+    e = document.getElementById("audio-options");
+    new_file = e.options[e.selectedIndex].text
+    audio = document.getElementById("audio")
+    audio.src = "static/media/" + new_file;
+
+    audio.load()
+
+    p = document.getElementById("audio-valence")
+    p2 = document.getElementById("audio-measures")
+    p.innerHTML = new_file.substring(0, new_file.length - 4) + "_valence = {"
+    p2.innerHTML = new_file.substring(0, new_file.length - 4) + "_measures = {"
+}
+
+// function finishValenceAnnotation() {
+//     // downloads text file of valence information and disables buttons
+//     if (confirm("Download?") === true) {
+//         $("#audio-container :input").attr("disabled", true)
+//         $("#audio").attr("controls", false)
+//         $("#finish-annotate").attr("disabled", true)
+//         $("#valence-button-restart").attr("disabled", false)
+//
+//         file = new Blob([p.innerText], {type: "text/plain"})
+//
+//         select = document.getElementById("audio-options")
+//         name = select[select.selectedIndex].text
+//
+//         var a = document.createElement("a")
+//         a.href = window.URL.createObjectURL(file)
+//         a.download = name.substring(0, name.length-4) + "_annotation.txt"
+//         a.click()
+//     } else {
+//         text = "You canceled!";
+//     }
+// }
+
+function finishValenceAnnotation() {
+    if (confirm("Please confirm that you have completed your analysis") === true) {
+        p = document.getElementById("audio-valence")
+        e = document.getElementById("audio-options");
+        new_file = e.options[e.selectedIndex].text
+
+        const data = {
+            "piece": new_file,
+            "analysis": p.innerText
+        };
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }
+        fetch('/api', options)
+            .then(response => console.log(response.json()))
+            .catch(error => {
+                console.error('Error:', error)
+            })
+
+        $("#finish-annotate").attr("disabled", true)
+    }
+}
+
+document.onkeypress = function(e) {
+    if (e.keyCode === 49) { //1
+        $("#anger").click().focus()
+    } else if (e.keyCode === 50) { //2
+        $("#fear").click().focus()
+    } else if (e.keyCode === 51) { //3
+        $("#sadness").click().focus()
+    } else if (e.keyCode === 55) { //7
+        $("#irony").click().focus()
+    } else if (e.keyCode === 56) { //8
+        $("#love").click().focus()
+    } else if (e.keyCode === 57) { //9
+        $("#joy").click().focus()
+    }
+}
+
+// function secondaryEmotion(mainEmotion) {
+//     mainButton = document.getElementById(mainEmotion)
+//     mainButton.style.backgroundColor = "white"
+//     mainButton.style.color = "black"
+// }
+
+// $(document).on("keypress", "selector", keyboardEmotionButtons)
+addEventListener('playing', startAnnotate)
+addEventListener('pause', stopAnnotate)
+
+
+
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Datastore = require('nedb')
+
+const app = express()
+
+const portNumber = 8080
+app.listen(portNumber, () => console.log("listening at " + portNumber))
+app.use(express.static('public'))
+app.use(express.json({ limit: '1mb' }))
+
+const database = new Datastore('database.db')
+database.loadDatabase()
+
+app.post('/api', (request, response) => {
+    const data = request.body
+    data.analysis = data.analysis.split("\,\n")
+    data.timestamp = Date.now()
+
+    database.insert(data)
+    response.json({
+        status: "success"
+    });
+})
