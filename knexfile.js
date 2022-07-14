40316ad5ad3dd1e67813a53bbf522e201129f464
stephenHahn88 <89817088+stephenHahn88@users.noreply.github.com>
2022-07-14 19:15:20 -0400

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -7,33 +7,25 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    ssl: {
+      rejectUnauthorized: false
+    },
     connection: {
       filename: './data/test.db3'
     },
     useNullAsDefault: true,
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    native: true,
     ssl: {
       rejectUnauthorized: false
+    },
+    dialectOptions: {
+      ssl: true
     },
     pool: {
       min: 2,
