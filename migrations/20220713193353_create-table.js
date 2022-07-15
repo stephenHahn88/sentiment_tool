43507b09e4051ec2818878c47f30d0cc46ea66e6
stephenHahn88 <89817088+stephenHahn88@users.noreply.github.com>
2022-07-14 23:45:47 -0400

    Update 20220713193353_create-table.js

--- migrations/20220713193353_create-table.js
+++ migrations/20220713193353_create-table.js
@@ -6,8 +6,8 @@ exports.up = function(knex) {
   return knex.schema
       .createTable("analyses", tbl => {
           tbl.increments();
-          tbl.text("piece")
-          tbl.json("analysis")
+          tbl.text("piece").notNullable()
+          tbl.json("analysis").notNullable()
           tbl.timestamp(true, true)
       })
 };
