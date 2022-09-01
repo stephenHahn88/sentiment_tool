65f97b12f9207102d4913db82ba3880e107923c7
stephenHahn88 <89817088+stephenHahn88@users.noreply.github.com>
2022-09-01 14:57:12 -0400

    .

--- public/static/js/visualize_emotion.js
+++ public/static/js/visualize_emotion.js
@@ -58,7 +58,7 @@ async function displayPhraseRanges() {
   }
   test.innerText = JSON.stringify(ranges, null, "\t")
 
-  // download(JSON.stringify(ranges), 'test.txt', 'text/plain')
+  download(ranges, `${file_name.slice(0, -4)}.json`, 'application/json')
 }
 
 async function fetchAnalyses() {
