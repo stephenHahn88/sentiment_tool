2edc3fea06c30068cfae18a00def6cd15244b2f4
Rico Zhu <rico.zhu@duke.edu>
2023-03-14 13:52:07 -0400

    Added Draggable Charts

--- ui/src/components/progression/BarPlotInput.vue
+++ ui/src/components/progression/BarPlotInput.vue
@@ -1,32 +1,47 @@
 <template>
-    <Bar
-      id="input-bar"
-      :options="chartOptions"
-      :data="chartData"
-    />
+    <div>
+      <canvas id="chart"></canvas>
+    </div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+import { Chart, registerables } from 'chart.js'
+import 'chartjs-plugin-dragdata'
 
-let chartData = {
-            labels: ["anger", "fear", "sadness", "none", "irony", "love", "joy"],
-            datasets: [ { data: [1, 0.8, 0.6, 0.4, 0.2, 0.2, 0.2] } ]
+let chartOptions = {
+            type: "bar",
+            data: {
+                labels: ["anger", "fear", "sadness", "none", "irony", "love", "joy"],
+                datasets: [ { data: [1, 0.8, 0.6, 0.4, 0.2, 0.2, 0.2] } ]
+            },
+            options: {
+                responsive: true,
+                plugins: {dragData: true}
+            }
         }
 
 export default {
-    name: 'BarChart',
-    components: { Bar },
     data() {
         return {
-        chartData,
-        chartOptions: {
-            responsive: true
+            chartOptions
         }
+    },
+    mounted() {
+        Chart.register(...registerables)
+        this.createChart('chart', this.chartOptions)
+    },
+    methods: {
+        createChart(chartId, chartData) {
+            const ctx = document.getElementById(chartId)
+            const myChart = new Chart(ctx, {
+                type: chartData.type,
+                data: chartData.data,
+                options: chartData.options,
+            })
         }
     }
 }
 </script>
+<style>
+</style>
