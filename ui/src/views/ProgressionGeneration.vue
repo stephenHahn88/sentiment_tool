89ab3f3af8340153ebb374a9883f148ca6ec6eea
Rico Zhu <rico.zhu@duke.edu>
2023-03-14 11:40:40 -0400

    Added Sidebar to ProgressionGeneration.vue

--- ui/src/views/ProgressionGeneration.vue
+++ ui/src/views/ProgressionGeneration.vue
@@ -1,61 +1,22 @@
 <template>
     <b-container>
-      <Instructions></Instructions>
-      <b-row>
-        <b-col>
-          <PieceSelection
-            @choiceUpdated="handleChoiceUpdated"
-          ></PieceSelection>
-        </b-col>
-        <b-col>
-          <audio
-              controls
-              id="audio"
-              :src="currAudioSrc"
-              class="m-4"
-          >
-          </audio>
-        </b-col>
-      </b-row>
-      <b-row>
-        <EmotionButtons
-          @emotionUpdate="handleEmotionUpdate"
-        ></EmotionButtons>
-      </b-row>
-      <b-row class="my-5">
-        <OutputPanel
-          :current-emotion="currEmotion"
-          :current-file="currAudioSrc"
-        ></OutputPanel>
-      </b-row>
+      <div>
+        <b-sidebar id="sidebar-1" title="Sidebar" shadow visible no-header-close>
+          <div class="px-3 py-2">
+            <p>
+              Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+              in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+            </p>
+            <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+          </div>
+        </b-sidebar>
+      </div>
     </b-container>
   </template>
   
   <script setup lang="ts">
   import {onMounted, ref, Ref, watch} from "vue"
   import Instructions from "@/components/Instructions.vue";
-  import PieceSelection from "@/components/PieceSelection.vue";
-  import EmotionButtons from "@/components/EmotionButtons.vue";
-  import OutputPanel from "@/components/OutputPanel.vue";
-  
-  let audioElement: HTMLAudioElement;
-  
-  onMounted(() => {
-    audioElement = document.getElementById("audio") as HTMLAudioElement
-  })
-  
-  let currEmotion: Ref<string> = ref("none")
-  
-  let currAudioSrc = ref("src/media/op18_no1_mov4.mp3")
-  
-  function handleEmotionUpdate(emotion: string) {
-    currEmotion.value = emotion
-  }
-  
-  function handleChoiceUpdated(path: string) {
-    currAudioSrc.value = path
-  }
-  
   
   </script>
   
