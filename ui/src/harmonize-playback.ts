b9474e2f78c361b79020e54d3bd85dbca0296095
Rico Zhu <ricozhu2003@gmail.com>
2024-01-22 11:00:26 -0500

    Created harmonize-playback.ts

---
+++ ui/src/harmonize-playback.ts
@@ -0,0 +1,5 @@
+import * as Tone from "tone";
+import { ToneAudioNode } from "tone";
+import RNSToPitches from "/static/data/notes_from_chord.json"
+import notesToKeys from "/static/data/notes_to_keys.json"
+
